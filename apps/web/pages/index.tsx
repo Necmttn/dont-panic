778a462b73e8b7e89dd5bdaafc958a2b96ade10e
Necmettin Karakaya <necmettin.karakaya@gmail.com>
2022-01-17 20:02:53 +0700

    add a index

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,12 +1,55 @@
 import { Button } from "@dont-panic/ui";
+import fs from 'fs';
+import _ from "lodash";
+import Link from "next/link";
+import path from 'path';
 
-export default function Web() {
+export default function Web(props) {
+  console.log(props)
   return (
     <div className="min-h-screen">
       <div className="bg-black h-screen flex items-center justify-center flex-col">
         <h1 className="text-[10rem] font-mono text-yellow-300 font-bold leading-none">DON{"'"}T <br /> PANIC!</h1>
-        <Button />
+        <Button>
+          Start
+          </Button>
+          <div>
+            {props.paths.map(path => {
+              return (<div className="text-white">
+                <Link href={`/learn/${path.params.slug.join("/")}`}>
+                  {path.params.slug.join(" - ")}
+                </Link>
+                </div>
+                )
+            })}
+          </div>
       </div>
     </div>
   );
+}
+
+
+
+export const getStaticProps = async () => {
+  const a = fs
+  .readdirSync(path.join(process.cwd(), 'node_modules', '@dont-panic'))
+  .filter(packageName => packageName.startsWith('learn-'))
+  .map(learnPackageName => {
+    return fs.readdirSync(path.join(process.cwd(), 'node_modules', '@dont-panic', learnPackageName, 'content'))
+    .map(contentName => {
+      return [learnPackageName.replace('learn-', ''), contentName.replace('.mdx', '')]
+    })
+  })
+  const paths = _.flatten(a).map(slug => {
+    return {
+      params: {
+        slug
+      }
+    }
+  })
+  return {
+    props: {
+      paths
+    }
+  }
 }
